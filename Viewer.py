ad562fd136a397d0f4690227e1939c5cfd90ba8d
gfhorigin <98154676+gfhorigin@users.noreply.github.com>
2026-02-27 21:49:42 +0700

    Initial commit

---
+++ Viewer.py
@@ -0,0 +1,29 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+API_TOKEN = os.getenv('API_TOKEN')
+
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO)
+# Объект бота
+bot = Bot(token=os.getenv('API_TOKEN'))
+# Диспетчер
+dp = Dispatcher()
+
+# Хэндлер на команду /start
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    await message.answer("Hello!")
+
+# Запуск процесса поллинга новых апдейтов
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
